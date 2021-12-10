f4427a68a3bff247bf60998a5ec3fc87b2051ec7
Surobhi <surobhi1395@gmail.com>
2021-12-10 18:47:12 +0530

    application.properties file updates

--- src/main/java/com/project/employeecrud/config/SwaggerConfig.java
+++ src/main/java/com/project/employeecrud/config/SwaggerConfig.java
@@ -16,7 +16,7 @@ public class SwaggerConfig {
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.student.registration.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.project.employeecrud.controller"))
                 .paths(PathSelectors.any())
                 .build();
 
